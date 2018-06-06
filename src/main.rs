226ae9ce8816e2e2eea6158670a8793cef72c595
Matti Hänninen <matti@mjhanninen.com>
2018-06-06 21:54:32 +0300

    Add command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
-use std::env;
+extern crate clap;
+
 use std::io::{self, Write};
 
+use clap::{App, Arg};
+
 fn main() {
+    let matches = App::new("Pathcomp")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author("Matti Hänninen <matti@mjhanninen.com>")
+        .about(
+            "Concatenates, reorders, and removes duplication from PATH-like environment variables",
+        )
+        .arg(
+            Arg::with_name("PREFIX_RULE")
+                .short("p")
+                .long("prefix")
+                .takes_value(true)
+                .number_of_values(1)
+                .multiple(true)
+                .help("Bring elements matching this prefix rule forth"),
+        )
+        .arg(Arg::with_name("PATHVARS").multiple(true))
+        .get_matches();
+    let pathvars = matches
+        .values_of("PATHVARS")
+        .map_or(vec![], |v| v.into_iter().collect::<Vec<_>>());
     let mut output = Vec::new();
-    let mut args = env::args();
-    args.next();                // toss executable
-    for arg in args {
-        for path in arg.split(":") {
+    for pathvar in pathvars {
+        for path in pathvar.split(":") {
             if output.iter().all(|p| p != &path) {
                 output.push(path.to_owned());
             }
