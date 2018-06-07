9b70df1832f8991ce886ccbb2eef6d1f9b0ee08c
Matti Hänninen <matti@mjhanninen.com>
2018-06-07 19:59:14 +0300

    Add prefix rule based reordering

--- src/main.rs
+++ src/main.rs
@@ -43,16 +43,73 @@ fn main() {
     run(config);
 }
 
-fn run(config: Config) {
-    let paths = config
-        .pathvars
+#[derive(Clone, Debug, PartialOrd, PartialEq)]
+enum Match {
+    Exact,
+    Partial(usize),
+}
+
+fn match_prefix(prefix: &str, path: &str) -> Option<Match> {
+    if prefix == path {
+        Some(Match::Exact)
+    } else if path.starts_with(prefix) {
+        Some(Match::Partial(path.len() - prefix.len()))
+    } else {
+        None
+    }
+}
+
+fn match_rule(rules: &[&str], path: &str) -> usize {
+    rules
         .into_iter()
+        .enumerate()
+        .fold(None, |old: Option<(usize, Match)>, (i, r)| {
+            match (old, match_prefix(r, path).map(|m| (i, m))) {
+                (Some(old), Some(new)) => {
+                    if new.1 < old.1 {
+                        Some(new)
+                    } else {
+                        Some(old)
+                    }
+                }
+                (None, new) => new,
+                (old, None) => old,
+            }
+        })
+        .map(|(i, _)| i)
+        .unwrap_or(rules.len())
+}
+
+fn run(config: Config) {
+    let mut paths: Vec<(usize, &str)> = config
+        .pathvars
+        .iter()
         .flat_map(|pathvar| pathvar.split(":"))
         .fold(Vec::new(), |mut compressed, path| {
             if compressed.iter().all(|p| p != &path) {
                 compressed.push(path);
             }
             compressed
-        });
+        })
+        .into_iter()
+        .map(|path| (match_rule(&config.prefix_rules, path), path))
+        .collect();
+    paths.sort_by_key(|x| x.0);
+    let paths: Vec<&str> = paths.into_iter().map(|(_, path)| path).collect();
     io::stdout().write(paths.join(":").as_bytes()).unwrap();
+}
+
+#[cfg(test)]
+mod test {
+
+    #[test]
+    fn test_match_ordering() {
+        use super::Match;
+        assert!(Match::Exact < Match::Partial);
+        assert!(Match::Exact < Match::NoMatch);
+        assert!(Match::Partial < Match::NoMatch);
+        assert!(Match::Exact == Match::Exact);
+        assert!(Match::Partial == Match::Partial);
+        assert!(Match::NoMatch == Match::NoMatch);
+    }
 }
